1a8c8f95197880ffb069422f0c42a68fdb2f56fa
kiburei <akiburei@gmail.com>
2017-10-18 14:12:05 +0300

    create user model, address error 401 after sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions", registrations: "users/registrations" }
   devise_for :hotels, :controllers => { registrations: 'registrations' }
   devise_for :installs
   resources :facilities
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171018093317) do
+ActiveRecord::Schema.define(version: 20171018100332) do
 
   create_table "bookings", force: :cascade do |t|
     t.integer  "guests"
@@ -95,6 +95,23 @@ ActiveRecord::Schema.define(version: 20171018093317) do
     t.integer  "facility_id"
     t.index ["facility_id"], name: "index_photos_on_facility_id"
     t.index ["hotel_id"], name: "index_photos_on_hotel_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at",                          null: false
+    t.datetime "updated_at",                          null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
 end
