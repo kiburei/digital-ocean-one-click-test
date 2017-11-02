f516f4986e35186a9a223359c67cc9a9f057764d
kiburei <akiburei@gmail.com>
2017-11-02 13:26:22 +0300

    Impelemt log in with facebook

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :users, path: 'users', controllers: { sessions: "users/sessions", registrations: "users/registrations" }
+  devise_for :users, path: 'users', controllers: { sessions: "users/sessions", registrations: "users/registrations", omniauth_callbacks: "users/omniauth_callbacks" }
   devise_for :hotels, :controllers => { registrations: 'registrations' }
   devise_for :installs
   resources :facilities do
