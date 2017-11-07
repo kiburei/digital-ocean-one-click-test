4549c80a54d3d6bd75cbbd24f40c1402c47f31b4
kiburei <akiburei@gmail.com>
2017-11-07 11:50:07 +0300

    add active admin dashboard

--- app/admin/dashboard.rb
+++ app/admin/dashboard.rb
@@ -12,22 +12,34 @@ ActiveAdmin.register_page "Dashboard" do
 
     # Here is an example of a simple dashboard with columns and panels.
     #
-    # columns do
-    #   column do
-    #     panel "Recent Posts" do
-    #       ul do
-    #         Post.recent(5).map do |post|
-    #           li link_to(post.title, admin_post_path(post))
-    #         end
-    #       end
-    #     end
-    #   end
+    columns do
 
-    #   column do
-    #     panel "Info" do
-    #       para "Welcome to ActiveAdmin."
-    #     end
-    #   end
-    # end
+      column do
+        panel "Info" do
+          para "Welcome to ActiveAdmin."
+        end
+      end
+
+      column do
+        panel "Recent Facilities" do
+          ul do
+            Facility.last(5).map do |facility|
+              li link_to(facility.name, admin_facility_path(facility))
+            end
+          end
+        end
+      end
+
+      column do
+        panel "Recent Bookings" do
+          ul do
+            Booking.last(5).map do |booking|
+              li link_to(booking.facility.name, admin_booking_path(booking))
+            end
+          end
+        end
+      end
+
+    end
   end # content
 end
