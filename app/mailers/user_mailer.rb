cd084eae0b307b950dc18964f5112da3d07362b9
kiburei <akiburei@gmail.com>
2017-11-13 15:41:13 +0300

    add log in styles

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -4,4 +4,9 @@ class UserMailer < ApplicationMailer
     @facility = facility
     mail to: @facility.hotel.email, subject: "A booking has been made"
   end
+
+  def confirmation(booking)
+    mail to: booking.user.email, subject: "Facility has been reserved"
+  end
+
 end
