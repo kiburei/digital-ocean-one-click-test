8370cd66f25b88403634fdf7f16d8d919d6d4f0d
kiburei <akiburei@gmail.com>
2017-11-15 09:43:54 +0300

    fix migration conflict

--- db/migrate/20171108082031_change_column_status_bookings.rb
+++
@@ -1,5 +0,0 @@
-class ChangeColumnStatusBookings < ActiveRecord::Migration[5.0]
-  def change
-    change_column :bookings, :status, :string, default: false
-  end
-end
--- db/migrate/20171108082613_drop_column_status.rb
+++
@@ -1,5 +0,0 @@
-class DropColumnStatus < ActiveRecord::Migration[5.0]
-  def change
-    remove_column :bookings, :status
-  end
-end
